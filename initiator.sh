fbe0af791fd778705546984d49340b6bce8b9f3f
kaori14112 <57876065+kaori14112@users.noreply.github.com>
2020-04-07 14:52:28 +0700

    Update initiator.sh

--- initiator.sh
+++ initiator.sh
@@ -3,7 +3,12 @@ mkdir -p /opt/backup/SCRIPT_ZIBRA_BACKUP_ALL_ACCOUNTS/
 cp {zimbra_backup_allaccounts.sh}{zimbra_backup_account_password.sh} /opt/backup/SCRIPT_ZIBRA_BACKUP_ALL_ACCOUNTS/
 chown -R zimbra:zimbra /opt/backup/SCRIPT_ZIBRA_BACKUP_ALL_ACCOUNTS/*
 chmod +x /opt/backup/SCRIPT_ZIBRA_BACKUP_ALL_ACCOUNTS/*
-chmod +x runbackup-all.sh
 mkdir -p /opt/zimbra/backup/mailbox/
 chown -R zimbra:zimbra /opt/zimbra/backup/mailbox/
-mkdir -o /home/backup
+mkdir -p /home/backup
+mv runbackup-all.sh /home/backup/
+chmod +x /home/backup/runbackup-all.sh
+crontab -l > backupzimbra
+echo "0 22 * * * /root/backup/runbackup-all.sh" >> backupzimbra
+crontab backupzimbra
+rm -rf backupzimbra
